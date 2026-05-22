5e5523da0a46fb0f70bc24f68ffa82a2c88c8b17
mdalmarco <mdalmarcoss@gmail.com>
2026-05-22 19:26:50 -0300

    fix: increase Nav logo size h-24 desktop / h-16 mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,7 +16,7 @@ export default function Nav() {
           <img
             src="/images/logo_sw_timbo.jpg"
             alt="Techstars Startup Weekend Timbó"
-            className="h-16 w-auto max-md:h-12 [mix-blend-mode:multiply]"
+            className="h-24 w-auto max-md:h-16 [mix-blend-mode:multiply]"
           />
         </a>
 
